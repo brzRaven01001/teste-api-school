09308928d89553125a263b5d9a1c629baf721c96
MuriloLF <124938721+brzRaven01001@users.noreply.github.com>
2025-03-18 21:42:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,69 @@
 from flask import Flask, jsonify, request
 
 dici = {
-    "alunos": [
+    "professores": [
         {
             "id": 1,
-            "nome": "Lucas Silva",
-            "idade": "19"
+            "nome": "Estevão Ferreira",
+            "idade": 43,
+            "data_nascimento": "01/07/1983",
+            "disciplina": "Sistemas",
+            "salario": 3750.00
         }
     ],
 }
 
 app = Flask(__name__)
 
-# -------------------- ALUNOS -------------------- #
+# -------------------- PROFESSORES-------------------- #
 
-@app.route('/alunos', methods=['GET'])
-def get_alunos():
-    return jsonify(dici["alunos"])
+@app.route('/professores', methods=['GET'])
+def get_professores():
+    return jsonify(dici["professores"])
 
-@app.route('/alunos', methods=['POST'])
-def create_aluno():
+@app.route('/professores/<int:idProfessores>', methods=['GET'])
+def get_professor_by_id(idProfessores):
+    # Filtra o professor pelo ID
+    professor = next((prof for prof in dici["professores"] if prof["id"] == idProfessores), None)
+    
+    if professor:
+        return jsonify(professor)
+    return jsonify({"erro": "Professor não encontrado"}), 404
+
+@app.route('/professores', methods=['POST'])
+def create_professores():
     dados = request.json
-    if not dados.get("nome") or not dados.get("idade"):
-        return jsonify({"erro": "Nome e idade são obrigatórios"}), 400
+    if not dados.get("id") or not dados.get("nome"):
+        return jsonify({"erro": "Nome e id são obrigatórios"}), 400
 
-    novo_id = max([aluno["id"] for aluno in dici["alunos"]], default=0) + 1
-    novo_aluno = {
+    novo_id = max([professor["id"] for professor in dici["professores"]], default=0) + 1
+    novo_professor = {
         "id": novo_id,
         "nome": dados["nome"],
-        "idade": dados["idade"]
     }
-    dici['alunos'].append(novo_aluno)
-    return jsonify(novo_aluno), 201  # Status 201 <---
+    dici['professores'].append(novo_professor)
+    return jsonify(novo_professor), 201  # Status 201 <---
 
-@app.route("/alunos/<int:idAluno>", methods=['PUT'])
-def update_aluno(idAluno):
-    alunos = dici["alunos"]
-    for aluno in alunos:
-        if aluno['id'] == idAluno:
-            dados = request.json
-            aluno['nome'] = dados.get("nome", aluno['nome'])
-            aluno['idade'] = dados.get("idade", aluno['idade'])
-            return jsonify(aluno)
-    return jsonify({"erro": "Aluno não encontrado"}), 404
+@app.route("/professores/<int:idProfessores>", methods=['PUT'])
+def update_professores(idProfessores):
+    professores = next((professores for professores in dici["professores"] if professores["id"] == idProfessores), None)
+    
+    if professores:
+        dados = request.json
+        professores['nome'] = dados.get("nome", professores['nome'])
+        return jsonify(professores)
+    
+    return jsonify({"erro": "Professor não encontrado"}), 404
 
-@app.route("/alunos/<int:idAluno>", methods=['DELETE'])
-def delete_aluno(idAluno):
-    alunos = dici["alunos"]
-    for aluno in alunos:
-        if aluno['id'] == idAluno:
-            dici["alunos"].remove(aluno)
-            return jsonify({"mensagem": "Aluno removido com sucesso"}), 200
-    return jsonify({"erro": "Aluno não encontrado"}), 404
+@app.route("/professores/<int:idProfessores>", methods=['DELETE'])
+def delete_professores(idProfessores):
+    professor = next((prof for prof in dici["professores"] if prof["id"] == idProfessores), None)
+    
+    if professor:
+        dici["professores"].remove(professor)
+        return jsonify({"mensagem": "Professor removido com sucesso"}), 200
+    
+    return jsonify({"erro": "Professor não encontrado"}), 404
 
 
 if __name__ == '__main__':
