9a7656d8035a373906b2f70e75cdd4d44d73d3b6
Guilherme_Alexandre <guilhermedafranca002@gmail.com>
2025-04-08 09:45:09 -0300

    .

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, redirect, url_for
+from config import DevelopmentConfig
 from models.professores import professor_bp
 from models.alunos import alunos_bp
 from controllers.turmas import turmas_bp
 
 app = Flask(__name__, template_folder='templates')
-
+app.config.from_object(DevelopmentConfig)
 
 app.register_blueprint(professor_bp, url_prefix='/professor')
 app.register_blueprint(alunos_bp, url_prefix='/alunos')
