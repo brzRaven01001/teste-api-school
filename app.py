349343c1aab3604410c409de12d2dace5cdb448f
Kauan Pedrosa <57303016+kauanpedrosa@users.noreply.github.com>
2025-05-02 15:45:40 -0300

    app.py atualizado com o host e a porta

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host=app.config["HOST"], port=app.config["PORT"], debug=app.config["DEBUG"])
+    app.run(host="0.0.0.0", port=5000, debug=True)
